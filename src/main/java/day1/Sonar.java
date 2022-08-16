a2af739b8bcedcaf8e980bea22d8893843da3a7b
JoelDumont <joel.m.dumont@gmail.com>
2022-08-16 14:41:51 +0200

    Added new test to read multiple lines

--- src/main/java/day1/Sonar.java
+++ src/main/java/day1/Sonar.java
@@ -3,6 +3,9 @@ package day1;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Sonar {
     final String pathToFile;
@@ -11,12 +14,15 @@ public class Sonar {
         this.pathToFile = pathToFile;
     }
 
-    public String readFirstLine() {
-        try (final BufferedReader reader = new BufferedReader(new FileReader(pathToFile));) {
-            return reader.readLine();
-        } catch (IOException e){
+    public ArrayList<String> readLines(int nrOfLines) {
+        try (final BufferedReader reader = new BufferedReader(new FileReader(pathToFile))) {
+            final ArrayList<String> list = new ArrayList<>();
+            for (int i = 0; i < nrOfLines; i++) {
+                list.add(reader.readLine());
+            }
+            return list;
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
 }
--- src/test/java/day1/SonarTest.java
+++ src/test/java/day1/SonarTest.java
@@ -4,6 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class SonarTest {
     public static final String PATH_TO_FILE = "./src/test/resources/input.txt";
 
@@ -12,15 +16,28 @@ public class SonarTest {
         //arrange
         final Sonar sonar = new Sonar(PATH_TO_FILE);
         //act
-        final String result = sonar.readFirstLine();
+        final List<String> result = sonar.readLines(1);
         //assert
-        assertEquals(result,"182");
+        assertEquals(List.of("182"), result);
     }
 
     @Test
-    public void firstTwoLines_increasingOrDecreasing() {
+    public void readMultipleLines() {
         //arrange
+        final Sonar sonar = new Sonar(PATH_TO_FILE);
         //act
+        final List<String> result = sonar.readLines(2);
         //assert
+        assertEquals(Arrays.asList("182", "188"), result);
+    }
+
+    @Test
+    public void readAllLines() {
+        //arrange
+        final Sonar sonar = new Sonar(PATH_TO_FILE);
+        //act
+        final List<String> result = sonar.readLines();
+        //assert
+        assertEquals(2_000, result.size());
     }
 }
